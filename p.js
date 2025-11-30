616a2f15fb6167033bd9b56d5397ebf9d3afe6a9
SanikaShinde09 <sanikashinde0949@gmail.com>
2025-11-30 20:48:17 +0530

    add file

---
+++ p.js
@@ -0,0 +1,8 @@
+let age = 18;
+if(age >=18){
+    console.log("you are an adult");
+}else{
+      console.log("you are an minor");
+    }
+    
+
---
+++ server.js
@@ -0,0 +1,3 @@
+let fname = "hello"
+console.log(fname)
+
